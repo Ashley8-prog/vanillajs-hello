423e4c849890663bec78879b0d95c91c4ea30d7e
Ashley8-prog <131078848+Ashley8-prog@users.noreply.github.com>
2023-07-04 02:27:20 +0000

    ExcuseGenerator
    clear

--- src/app.js
+++ src/app.js
@@ -6,6 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#the-excuse").innerHTML = randomNumber();
+};
+let randomNumber = () => {
+  let Pronombre = ["Su", "Él"];
+  let quien = ["cobaya", "vecino", "gato", "mamá"];
+  let acción = ["se comió", "rompió", "rasgó"];
+  let posesión = ["mi tarea", "mi pantalon", "mis zapatos", "mi celular"];
+  let cuando = ["anoche", "Hoy", "en la manaña", "ayer"];
+
+  let pronIndex = Math.floor(Math.random() * Pronombre.length);
+  let quienIndex = Math.floor(Math.random() * quien.length);
+  let AccionIndex = Math.floor(Math.random() * acción.length);
+  let posesionIndex = Math.floor(Math.random() * posesión.length);
+  let cuandoIndex = Math.floor(Math.random() * cuando.length);
+
+  return (
+    Pronombre[pronIndex] +
+    " " +
+    quien[quienIndex] +
+    " " +
+    acción[AccionIndex] +
+    " " +
+    posesión[posesionIndex] +
+    " " +
+    cuando[cuandoIndex]
+  );
 };
